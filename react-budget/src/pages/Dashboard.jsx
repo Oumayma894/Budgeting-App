dcae571cee1bf2534e5d55fba17de9c276e8baf9
Oumayma894 <oumaymaelhammouchi58@gmail.com>
2026-02-18 23:41:05 +0000

    Handling Multiple Form Submissions

--- react-budget/src/pages/Dashboard.jsx
+++ react-budget/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { redirect } from "react-router-dom";
 
 // helper functions
-import { fetchData } from '../helpers'
+import { createBudget, fetchData } from '../helpers'
 import { useLoaderData } from 'react-router-dom';
 
 import { toast } from 'react-toastify';
@@ -20,13 +20,29 @@ export function dashboardLoader(){
 // action
 export async function dashboardAction({ request }) {
   const data = await request.formData();
-  const formData = Object.fromEntries(data)
-  try{
-  localStorage.setItem("userName", JSON.stringify(formData.userName))
-  return toast.success(`Welcome, ${formData.userName}`)
-  }catch(e){
-    throw new Error("There was a problem creating your account.")
+  const {_action, ...values} = Object.fromEntries(data)
+  // new user submission 
+  if(_action === "newUser"){
+     try{
+      localStorage.setItem("userName", JSON.stringify(values.userName))
+      return toast.success(`Welcome, ${values.userName}`)
+    }catch(e){
+     throw new Error("There was a problem creating your account.")
+   }
   }
+
+  if(_action === "createBudget"){
+    try{
+      createBudget({
+        name: values.newBudget,
+        amount: values.newBudgetAmount,
+      })
+      return toast.success("Budget created!")
+    }catch(e){
+      throw new Error("There was a problem creating your budget.")
+    }
+  }
+  
 }
 
 
