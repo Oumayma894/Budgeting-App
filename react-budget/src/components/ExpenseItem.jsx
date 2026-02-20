2830a005c6f4114da0355a5c3c31855472be296d
Oumayma894 <oumaymaelhammouchi58@gmail.com>
2026-02-20 21:17:06 +0000

    Adding Budget Links

--- react-budget/src/components/ExpenseItem.jsx
+++ react-budget/src/components/ExpenseItem.jsx
@@ -1,13 +1,48 @@
 import React from 'react'
+
 import { formatCurrency } from '../helpers'
-import { formatDateToLocaleString } from '../helpers'
+import { formatDateToLocaleString, getAllMatchingItems } from '../helpers'
+
+import { Link, useFetcher } from 'react-router-dom'
+
+import { TrashIcon } from '@heroicons/react/24/solid'
 
 const ExpenseItem = ({ expense }) => {
+  const fetcher = useFetcher();
+
+  const budget = getAllMatchingItems ({
+    category: "budgets",
+    key: "id",
+    value: expense.budgetId
+  })[0];
+
   return (
     <>
     <td>{expense.name}</td>
     <td>{formatCurrency(expense.amount)}</td>
     <td>{formatDateToLocaleString(expense.createAt)}</td>
+    <td>
+  <Link 
+    to={`/budget/${budget.id}`} 
+    style={{ "--accent": budget.color }}
+  >
+    {budget.name}
+  </Link>
+</td>
+  <td>
+    <fetcher.Form method='post'>
+      {/* What to do with it  */}
+    <input type="hidden" name='_action' value="deleteExpense"/> 
+      {/* the actual data ID that I need to delete */}
+    <input type="hidden" name='expenseId' value={expense.id}/>
+    <button type='submit'
+    className='btn btn--warning'
+    aria-label={`Delete ${expense.name} expense`}>
+      <TrashIcon width={20} />
+    </button>
+    </fetcher.Form>
+  </td>
+
     </>
   )
 }
